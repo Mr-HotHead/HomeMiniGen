6fb0c69f4597a045fdb3d9473f7139df17cfa118
Noobita69 <77660629+Noobita69@users.noreply.github.com>
2021-02-13 18:39:00 +0530

    Add files via upload

---
+++ ghomegene.py
@@ -0,0 +1,76 @@
+import random
+
+print("""
+         _   _  _____  __  __  ____     ___  _____  ____  ____     ___  ____  _  
+        ( )_( )(  _  )(  \/  )( ___)   / __)(  _  )(  _ \( ___)   / __)( ___)( \( )
+         ) _ (  )(_)(  )    (  )__)   ( (__  )(_)(  )(_) ))__)   ( (_-. )__)  )  ( 
+        (_) (_)(_____)(_/\/\_)(____)   \___)(_____)(____/(____)   \___/(____)(_)\_)""")
+
+print()
+print('             -:::- GOOGLE HOME MINI CODE GENERATOR -:::-        ')
+print('           --+-- Script by @Trustnoonekillanyone [Telegram] --+--        ')
+print()
+print("""    NOTE:These codes are for the Google Home Mini flipkart event happening in India.
+                    This even will end at 25Feb2020. So after 25Feb these codes are waste
+    This tool will give generated codes. You need to check the codes one by one on the flipkart site.You may get a real code """)
+print()
+print()
+i = 0
+count = int(input("Enter The Number of Codes you want: "))
+
+
+while i < count :
+        digit_3 = ('A','B','C','D','E','F','G','H','I','J','K','L','M',
+                   'N','O','P','Q','R','S','T','U','V','W','X','Y','Z')
+
+        digit_5 = ('A','B','C','D','E','F','G','H','I','J','K','L','M',
+                   'N','O','P','Q','R','S','T','U','V','W','X','Y','Z',
+                   'a','b','c','d','e','f','g','h','i','j','k','l','m',
+                   'n','o','p','q','r','s','t','u','v','w','x','y','z',
+                   '1','2','3','4','5','6','7','8','9','0')
+
+        digit_6 = ('A','B','C','D','E','F','G','H','I','J','K','L','M',
+                   'N','O','P','Q','R','S','T','U','V','W','X','Y','Z',
+                   'a','b','c','d','e','f','g','h','i','j','k','l','m',
+                   'n','o','p','q','r','s','t','u','v','w','x','y','z',
+                   '1','2','3','4','5','6','7','8','9','0')
+
+        digit_7 = ('A','B','C','D','E','F','G','H','I','J','K','L','M',
+                   'N','O','P','Q','R','S','T','U','V','W','X','Y','Z',
+                   'a','b','c','d','e','f','g','h','i','j','k','l','m',
+                   'n','o','p','q','r','s','t','u','v','w','x','y','z',
+                   '1','2','3','4','5','6','7','8','9','0')
+
+        digit_8 = ('A','B','C','D','E','F','G','H','I','J','K','L','M',
+                   'N','O','P','Q','R','S','T','U','V','W','X','Y','Z',
+                   'a','b','c','d','e','f','g','h','i','j','k','l','m',
+                   'n','o','p','q','r','s','t','u','v','w','x','y','z',
+                   '1','2','3','4','5','6','7','8','9','0')
+
+        digit_9 = ('A','B','C','D','E','F','G','H','I','J','K','L','M',
+                   'N','O','P','Q','R','S','T','U','V','W','X','Y','Z',
+                   'a','b','c','d','e','f','g','h','i','j','k','l','m',
+                   'n','o','p','q','r','s','t','u','v','w','x','y','z',
+                   '1','2','3','4','5','6','7','8','9','0')
+
+        digit_10 = ('A','B','C','D','E','F','G','H','I','J','K','L','M',
+                    'N','O','P','Q','R','S','T','U','V','W','X','Y','Z',
+                    'a','b','c','d','e','f','g','h','i','j','k','l','m',
+                    'n','o','p','q','r','s','t','u','v','w','x','y','z',
+                    '1','2','3','4','5','6','7','8','9','0')
+
+        random_digit_3 = random.choice(digit_3)
+        random_digit_5 = random.choice(digit_5)
+        random_digit_6 = random.choice(digit_6)
+        random_digit_7 = random.choice(digit_7)
+        random_digit_8 = random.choice(digit_8)
+        random_digit_9 = random.choice(digit_9)
+        random_digit_10 = random.choice(digit_10)
+
+        total = f"AM{random_digit_3}z{random_digit_5}{random_digit_6}{random_digit_7}{random_digit_8}{random_digit_9}{random_digit_10}"
+        print(total)
+        i = i + 1
+print()
+print("""   Thanks for using our tool. Hope you may get the Google Home Mini by these codes. 
+            Our Telegram channel @Technicalscams
+            Script by @Trustnoonekillanyone aka @Noobita69 [Telegram]""")
